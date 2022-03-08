8df55c1ce1fc82cdd9a670a04b35c66abe254fb4
Santhosh908 <santhoshyavd@gmail.com>
2022-03-08 18:45:03 +0530

    added  Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
 
 function App() {
-  var name="santhosh";
   return (
     <div className="App">
-      <h1>This is me {name}</h1>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header=()=>{
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      <button className="btn">add</button>
+    </header>
+  )
+}
+
+export default Header
